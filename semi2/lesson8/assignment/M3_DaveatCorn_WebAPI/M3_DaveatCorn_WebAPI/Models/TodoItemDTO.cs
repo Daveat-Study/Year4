fd62f1e1bbf50823fac59153fae13488211db80a
DaveatCor <condaveat@gmail.com>
2023-03-09 22:40:56 +0700

    finish assignment, 9/Mar/23

---
+++ semi2/lesson8/assignment/M3_DaveatCorn_WebAPI/M3_DaveatCorn_WebAPI/Models/TodoItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace M3_DaveatCorn_WebAPI.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
